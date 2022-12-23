fc33200d1b572f331d0bc6e35cd3676b594b02a9
shwtnair1211 <Shweta.Nair@wolterskluwer.com>
2022-12-23 14:34:56 +0100

    fix build issue

--- src/RCTree.js
+++ src/RCTree.js
@@ -1,55 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import {
-  ControlledTreeEnvironment,
   Tree,
   StaticTreeDataProvider,
   UncontrolledTreeEnvironment,
 } from "react-complex-tree";
-import { longTree } from "./demoData";
 import "react-complex-tree/lib/style.css";
 import initialData from "./myjsonfile.json";
-const mockApiCall = async (nextId) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        [nextId]: {
-          index: nextId,
-          canMove: true,
-          isFolder: true,
-          children: [],
-          data: "test child",
-          canRename: true,
-        },
-      });
-    }, 100);
-  });
-};
+// const mockApiCall = async (nextId) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve({
+//         [nextId]: {
+//           index: nextId,
+//           canMove: true,
+//           isFolder: true,
+//           children: [],
+//           data: "test child",
+//           canRename: true,
+//         },
+//       });
+//     }, 100);
+//   });
+// };
 
 const SingleTree = () => {
-  const [expandedItems, setExpandedItems] = useState([]);
-  const [treeData, setTreeData] = useState(initialData);
-  console.log(treeData);
-  const handleExpand = (item) => {
-    const nextId = Math.ceil(Math.random() * 100);
-    mockApiCall(nextId)
-      .then((result) => {
-        console.log(result);
-        setExpandedItems([...expandedItems, item.index]);
+  //   const [expandedItems, setExpandedItems] = useState([]);
+  //   const [treeData, setTreeData] = useState(initialData);
+  //   const handleExpand = (item) => {
+  //     const nextId = Math.ceil(Math.random() * 100);
+  //     mockApiCall(nextId)
+  //       .then((result) => {
+  //         console.log(result);
+  //         setExpandedItems([...expandedItems, item.index]);
 
-        setTreeData({
-          ...treeData,
-          ...result,
-          [item.index]: { ...item, children: [nextId] },
-        });
-      })
-      .catch((err) => console.error(err));
-  };
-  console.log("ex", expandedItems);
-  console.log();
+  //         setTreeData({
+  //           ...treeData,
+  //           ...result,
+  //           [item.index]: { ...item, children: [nextId] },
+  //         });
+  //       })
+  //       .catch((err) => console.error(err));
+  //   };
   return (
     <UncontrolledTreeEnvironment
       dataProvider={
-        new StaticTreeDataProvider(treeData, (item, data) => ({
+        new StaticTreeDataProvider(initialData, (item, data) => ({
           ...item,
           data,
         }))
--- src/RSTree.js
+++ src/RSTree.js
@@ -1,65 +1,71 @@
-import React, { useState } from "react";
-import {
-  Tree,
-  getBackendOptions,
-  MultiBackend,
-} from "@minoru/react-dnd-treeview";
-import { DndProvider } from "react-dnd";
-const TreeComp = () => {
-  const [treeData, setTreeData] = useState([
-    { id: 1, parent: 0, droppable: true, text: "Globe" },
-  ]);
-  const handleDrop = (newTreeData, options) => setTreeData(newTreeData);
+// import React, { useState } from "react";
+// import {
+//   Tree,
+//   getBackendOptions,
+//   MultiBackend,
+// } from "@minoru/react-dnd-treeview";
+// import { DndProvider } from "react-dnd";
+// const TreeComp = () => {
+//   const [treeData, setTreeData] = useState([
+//     { id: 1, parent: 0, droppable: true, text: "Globe" },
+//   ]);
+//   const handleDrop = (newTreeData, options) => setTreeData(newTreeData);
 
-  const mockApiCall = async (parentId) => {
-    console.log(parentId);
-    return new Promise((resolve) => {
-      const nextId = Math.ceil(Math.random() * 100);
-      resolve([
-        {
-          id: `${nextId}`,
-          text: `child-${nextId}`,
-          parent: parentId,
-          droppable: true,
-        },
-        {
-          id: `${nextId + 1}`,
-          text: `child-${nextId + 1}`,
-          parent: parentId,
-          droppable: true,
-        },
-      ]);
-    });
-  };
-  const makeApiCall = mockApiCall;
-  const handleChange = (ids) => {
-    let parentId = ids[ids.length - 1];
-    console.log(parentId);
-    makeApiCall(parentId)
-      .then((result) => {
-        console.log(result);
-        setTreeData(treeData.concat(result));
-      })
-      .catch((err) => console.error(err));
-  };
-  return (
-    <DndProvider backend={MultiBackend} options={getBackendOptions()}>
-      <Tree
-        tree={treeData}
-        rootId={0}
-        onDrop={handleDrop}
-        render={(node, { depth, isOpen, onToggle }) => (
-          <div style={{ marginLeft: depth * 10 }}>
-            {node.droppable && (
-              <span onClick={onToggle}>{isOpen ? "[-]" : "[+]"}</span>
-            )}
-            {node.text}
-          </div>
-        )}
-        onChangeOpen={handleChange}
-      />
-    </DndProvider>
-  );
+//   const mockApiCall = async (parentId) => {
+//     console.log(parentId);
+//     return new Promise((resolve) => {
+//       const nextId = Math.ceil(Math.random() * 100);
+//       resolve([
+//         {
+//           id: `${nextId}`,
+//           text: `child-${nextId}`,
+//           parent: parentId,
+//           droppable: true,
+//         },
+//         {
+//           id: `${nextId + 1}`,
+//           text: `child-${nextId + 1}`,
+//           parent: parentId,
+//           droppable: true,
+//         },
+//       ]);
+//     });
+//   };
+//   const makeApiCall = mockApiCall;
+//   const handleChange = (ids) => {
+//     let parentId = ids[ids.length - 1];
+//     console.log(parentId);
+//     makeApiCall(parentId)
+//       .then((result) => {
+//         console.log(result);
+//         setTreeData(treeData.concat(result));
+//       })
+//       .catch((err) => console.error(err));
+//   };
+//   return (
+//     <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+//       <Tree
+//         tree={treeData}
+//         rootId={0}
+//         onDrop={handleDrop}
+//         render={(node, { depth, isOpen, onToggle }) => (
+//           <div style={{ marginLeft: depth * 10 }}>
+//             {node.droppable && (
+//               <span onClick={onToggle}>{isOpen ? "[-]" : "[+]"}</span>
+//             )}
+//             {node.text}
+//           </div>
+//         )}
+//         onChangeOpen={handleChange}
+//       />
+//     </DndProvider>
+//   );
+// };
+
+// export default TreeComp;
+
+const RSTree = () => {
+  return <div>Commented</div>;
 };
 
-export default TreeComp;
+export default RSTree;
